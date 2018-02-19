4853d1db29d9317487c7006a65f6aef6d8781df0
Joe Reynolds <joereynolds952@gmail.com>
2018-02-19 20:23:40 +0000

    make demo custom check less intrusive

--- checks/custom/is_bogans_alive.sh
+++ checks/custom/is_bogans_alive.sh
@@ -1,8 +1,8 @@
 function check
 {
-    result=$(ping -w 5 bogans.uk | wc -l)
+    result=$(curl -s bogans.uk | grep 'Instagram' | wc -l)
 
-    if [[ "$result" -eq 10 ]]; then
+    if [[ "$result" -eq 1 ]]; then
         echo 0
     else
         echo 1
