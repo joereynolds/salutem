75b8c08a989ba985422de1dc4b8e458d50870283
Joe Reynolds <joereynolds952@gmail.com>
2018-02-20 18:21:44 +0000

    add some options to healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,25 +1,75 @@
+#!/bin/bash
 set -o errexit
 
 [ ! -d ~/.config/salutem/checks ] && echo 'No custom checks found, you can write your own in ~/.config/salutem/checks'
 
+main() {
 
-function main
-{
-    check
+    if [ "$1" == "--help" ]; then
+        display_help
+        exit 0
+    fi
+    if [ "$1" != '--help' ] &&
+       [ "$1" != '--colored' ] &&
+       [ ! -z "$1" ]; then
+        echo 'Incorrect option passed, try ./healthcheck.sh --help for more information'
+        exit 0
+    fi
+
+    check $1
 }
 
-function check
-{
+display_help() {
+    echo '
+    Usage: ./healthcheck.sh [OPTION]
+    Run checks and output to standard output.
+
+      --help     display this help and exit
+      --colored  display the results with ANSI color codes
+
+    Examples:
+      ./healthcheck.sh Perform all checks in your ~/.config directory and checks directory.'
+}
+
+check() {
     for file in checks/**/*.sh ~/.config/salutem/checks/*.sh; do
          source $file
 
          # Calls the check method from each script
          if [[ "$(check)" -eq 0 ]]; then
-             printf "\033[0;32m✔ \033[0m [$file]\n"
+
+             if [ "$1" == '--colored' ]; then
+                 print_coloured_success $file
+             else
+                 print_plain_success $file
+             fi
+
          else
-             printf "\033[0;31m✖ \033[0m [$file]\n"
+
+             if [ "$1" == '--colored' ]; then
+                 print_coloured_failure $file
+             else
+                 print_plain_failure $file
+             fi
+
          fi
     done
 }
 
-main
+print_coloured_success() {
+    printf "\033[0;32m✔ \033[0m [$1]\n"
+}
+
+print_coloured_failure() {
+    printf "\033[0;31m✖ \033[0m [$1]\n"
+}
+
+print_plain_success() {
+    echo "✔ [$1]"
+}
+
+print_plain_failure() {
+    echo "✖ [$1]"
+}
+
+main $1
