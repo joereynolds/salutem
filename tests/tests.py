d975f9af811e5668941429ba627d0b513a0cecad
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2018-02-22 14:54:59 +0000

    trying modules

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,11 @@
 import unittest
-
+from salutem.src import Checker
+from src import Checker
+# from src import Configuration
 
 class TestChecker(unittest.TestCase):
 
     def test_check_is_instantiable():
+        check = Checker(Configuration('~/.config/salutem/checks'))
         pass
 
